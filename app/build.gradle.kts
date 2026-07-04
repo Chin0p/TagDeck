7fae66499d458b98fbffe9ca225dbc8914882090
Chin0p <34020024+Chin0p@users.noreply.github.com>
2026-07-04 10:54:48 +0000

    fixed dependency issues

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.tagdeck"
-    compileSdk = 35
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.tagdeck"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
