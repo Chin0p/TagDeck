eae903f14204fcadcff521277f0031d131a706c0
Chin0p <34020024+Chin0p@users.noreply.github.com>
2026-07-04 11:41:37 +0000

    fixed some issues

--- app/src/main/java/com/tagdeck/Navigation.kt
+++ app/src/main/java/com/tagdeck/Navigation.kt
@@ -8,6 +8,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.tagdeck.data.DataRepository
+import com.tagdeck.data.DefaultDataRepository
 import com.tagdeck.ui.library.LibraryScreen
 import com.tagdeck.ui.library.LibraryScreenViewModel
 import com.tagdeck.ui.library.RenameScreen
